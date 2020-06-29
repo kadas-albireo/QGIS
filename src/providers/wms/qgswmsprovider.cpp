32d39d68fffa431b40b92f13d64501f972d5c654
Sandro Mani <manisandro@gmail.com>
2020-06-29 14:40:05 +0200

    Fix crash in QgsWmsProvider::htmlMetadata if layer is invalid

--- src/providers/wms/qgswmsprovider.cpp
+++ src/providers/wms/qgswmsprovider.cpp
@@ -2099,15 +2099,15 @@ QString QgsWmsProvider::htmlMetadata()
         metadata += QStringLiteral( "%1:%2<br>" ).arg( it.key(), it.value() );
       }
       metadata += QStringLiteral( "</td></tr>" );
-    }
 
-    // GetFeatureInfo Request Formats
-    metadata += QStringLiteral( "<tr><td>" ) %
-                tr( "Identify Formats" ) %
-                QStringLiteral( "</td>" ) %
-                QStringLiteral( "<td>" ) %
-                mTileLayer->infoFormats.join( QStringLiteral( "<br />" ) );
-    QStringLiteral( "</td></tr>" );
+      // GetFeatureInfo Request Formats
+      metadata += QStringLiteral( "<tr><td>" ) %
+                  tr( "Identify Formats" ) %
+                  QStringLiteral( "</td>" ) %
+                  QStringLiteral( "<td>" ) %
+                  mTileLayer->infoFormats.join( QStringLiteral( "<br />" ) ) %
+                  QStringLiteral( "</td></tr>" );
+    }
   }
   else
   {
